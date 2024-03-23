6fb47c66462b5a479ef76c9d9f0a7a5eeeb0d859
ErnestoDanielTafoyaMolina <edaniimol@gmail.com>
2024-03-22 21:15:31 -0600

    some routes added

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,22 @@
+export const register = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    }
+};
+export const login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    };
+};
+
+export const logout = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({ msg: error })
+    };
+}
